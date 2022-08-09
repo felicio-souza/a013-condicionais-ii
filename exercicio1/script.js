1fc870e2a735163a472306a622b96fbd129e901c
felicio-souza <felicio.fc13@gmail.com>
2022-08-09 11:44:01 -0300

    tarefa realizada com sucesso

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,20 @@
-const num = //seu numero aqui
+const num = prompt("Digite um numero:")
+
+
+if(num % 2 === 0 && num % 3 === 0){
+
+        console.log("Este numero é divisivel por 2 e 3")
+
+ } else if(num % 2 === 0){
+
+        console.log("Este numero é divisivel por 2")
+
+}  else if(num % 3 === 0){
+
+        console.log("Este numero é divisivel por 3")
+}     
+   else{
+        console.log("Numero não divisivel")
+   } 
+
 
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,29 @@
 //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+// if(nome === "José"){
+// 	console.log("Oi, Zé!");
+// } else {
+// 	console.log("Olá, "+nome);
+// }
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
+// if(idade >= 18){
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
+
+//reescrevendo codigo
+
+const nome = "José";
+const idade = 13;
+
+
+if(nome === "José"){
+
+	console.log("Oi, Zé!");
+
+	console.log(idade >= 18? "pode tirar carteira de motorista!" : "Ainda não pode tirar carteira de motorista");
+
+}else{
+	console.log("Olá, "+nome);
 }
